a1fa17a9dbc44b1305c0e2ade8011e2b2f464c27
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2020-03-30 10:50:42 -0400

    added question 1

---
+++ DSA-CTA.playground/Pages/Question 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//Question 1
+import Foundation
+
+// Q1.
+// Given a linked list, write a function to traverse it and print each value in order.
+class Node<T> {
+    var next: Node?
+    let val: T
+    init(val: T) {
+        self.val = val
+    }
+}
+
+// Print format:
+//```
+//-> 4 -> 9 -> 2 -> 3 -> nil
+//```
+
+print("Happy Monday")
